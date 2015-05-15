aa8aa61190f75343fe40e8e04d6291cf12ab633e
Alan Colon <github@alanc.net>
2015-05-15 09:28:57 -0600

    TextExistInStream

--- code.cs
+++ code.cs
@@ -1,8 +1,15 @@
 using System;
+using System.Collections.Generic;
 
 class Code {
     // Returns "Hello World!"
     public static string HelloWorld() {
+        throw new Exception("Not Implemented");
+    }
+
+    // Determines if text exists in a stream of text. 
+    // Assume the stream is from a 10 GB file.
+    public static bool IsTextInStream(IEnumerator<string> stream, string text) {
         throw new Exception("Not Implemented");
     }
 }
--- code/tests.cs
+++ code/tests.cs
@@ -1,7 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 static class Tests {
     public static void HelloWorldTest() {
         Assert.AreEqual("Hello World!", Code.HelloWorld());
     }
+    public static IEnumerable<string> Stream(string text) {
+        int chunksize = 10;
+        for (var i = 0; i < text.Length; i += chunksize) {
+            yield return text.Substring(i, Math.Min(text.Length - i, chunksize));
+        }
+    }
+    public static void IsTextInStreamTest() {
+        Assert.IsTrue(Code.IsTextInStream(Stream("Whatever FINDME Whatever").GetEnumerator(), "FINDME"));
+        Assert.IsTrue(Code.IsTextInStream(Stream("Whatever FINDME1238901238901230479823798 Whatever").GetEnumerator(), "FINDME1238901238901230479823798"));
+        Assert.IsFalse(Code.IsTextInStream(Stream("Whatever FINDME Whatever").GetEnumerator(), "Hello World"));
+    }
+    
 }
