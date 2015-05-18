64845baa44645c209d125528b1b4b8dc7cce5d48
Alan Colon <github@alanc.net>
2015-05-18 17:35:32 -0600

    Life JavaScript

--- code.js
+++ code.js
@@ -5,6 +5,16 @@ var code = {
     },
     isTextInStream: function(stream, text) {
         throw new Error("Not Implemented");
+    },
+    // For a space that is 'populated':
+    //   Each cell with one or no neighbors dies, as if by loneliness.
+    //   Each cell with four or more neighbors dies, as if by overpopulation.
+    //   Each cell with two or three neighbors survives.
+    // For a space that is 'empty' or 'unpopulated'
+    //   Each cell with three neighbors becomes populated.
+    iterateLife: function(field) {
+        throw new Error("Not Implemented");
+        
     }
 };
 module.exports = code;
