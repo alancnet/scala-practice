f2fceda7836f203f039a2c24a396049d7047cbe0
Slideshow Bob <slideshow.bob@ltme.com>
2015-10-14 13:53:42 -0600

    JavaScript tests

--- src/main/js/level1code.js
+++ src/main/js/level1code.js
@@ -53,31 +53,6 @@ var code = {
     // Reverses the array
     reverse: function (arr) {
         throw new Error("Not Implemented");
-    },
-    // Defines a singly-linked list implementation:
-    linkedList: {
-        head: null,
-        tail: null,
-        add: function (data) {
-            throw new Error("Not Implemented");
-        },
-        // Returns the smallest number in the list
-        min: function () {
-            throw new Error("Not Implemented");
-        },
-        // Returns the largest number in the list
-        max: function () {
-            throw new Error("Not Implemented")
-        },
-        // Returns the length of the list
-        length: function () {
-            throw new Error("Not Implemented");
-        }
-    },
-    // Returns a linked list object that contains 
-    // every integer starting at 'start' and ending at 'end'
-    countFromTo: function (start, end) {
-        throw new Error("Not Implemented");
     }
 };
 module.exports = code;
